ce7b1805ed7823ae21e9ec24f0c8a676c1c83322
injoker <marucube35@gmail.com>
2026-01-19 17:05:20 +0700

    chore: rotate keys and bump to v1.0.4

--- caido.config.ts
+++ caido.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   id: "burfisher",
   name: "Burfisher",
   description: "Scan HTTP traffic for exposed secrets using MongoDB Kingfisher",
-  version: "0.1.0",
+  version: "1.0.4",
   author: {
     name: "insomnia1102",
     url: "https://github.com/aleister1102/burfisher-caido",
